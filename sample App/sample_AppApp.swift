0e9791fc4dac1f4264c31db0bd458b7392bfbd85
hanatani-m <hanatani-m@pise.co.jp>
2022-03-03 16:54:29 +0900

    初期データアップ

---
+++ sample App/sample_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_AppApp.swift
+//  sample App
+//
+//  Created by pise on 2022/01/29.
+//
+
+import SwiftUI
+
+@main
+struct sample_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
